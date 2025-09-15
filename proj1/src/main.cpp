107f4811620978480860a8b2e69ff1fc53fce6e7
DocNos <j.varboncoeur@gmail.com>
2025-09-15 15:18:20 -0700

    Implementation complete, tests passing

--- proj1/src/main.cpp
+++ proj1/src/main.cpp
@@ -5,29 +5,58 @@
 
 int main(int argc, char *argv[])
 {
-    if(argc < 2){
-        std::cerr << "Not enough arguments" << std::endl;
-        return 1;
+    // Extract executable name from argv[0]
+    std::string exe_name = argv[0];
+    size_t last_slash = exe_name.find_last_of("/\\");
+    if (last_slash != std::string::npos) {
+        exe_name = exe_name.substr(last_slash + 1);
     }
-    switch(atoi(argv[1]))
+    
+    int part;
+    int arg_offset;
+    
+    // Determine part based on executable name
+    if (exe_name == "rot") {
+        part = 1;
+        arg_offset = 0;
+    } else if (exe_name == "sum") {
+        part = 2;
+        arg_offset = 0;
+    } else if (exe_name == "dot") {
+        part = 3;
+        arg_offset = 0;
+    } else if (exe_name == "prod") {
+        part = 4;
+        arg_offset = 0;
+    } else {
+        // Combined executable mode - part number from command line
+        if(argc < 2){
+            std::cerr << "Not enough arguments" << std::endl;
+            return 1;
+        }
+        part = atoi(argv[1]);
+        arg_offset = 1;
+    }
+
+    switch(part)
     {
         case(1):{
-            Rotate rot(atoi(argv[2]), atof(argv[3]), Read(argv[4]));
+            Rotate rot(atoi(argv[1+arg_offset]), atof(argv[2+arg_offset]), Read(argv[3+arg_offset]));
             rot.execute();
             rot.print();
         } break;
         case(2):{
-            SumUnity sum(atoi(argv[2]), atoi(argv[3]));
+            SumUnity sum(atoi(argv[1+arg_offset]), atoi(argv[2+arg_offset]));
             sum.execute();
             sum.print();
         }break;
         case(3):{
-            InnerProd inner(atoi(argv[2]), Read(argv[3]), Read(argv[4]));
+            InnerProd inner(atoi(argv[1+arg_offset]), Read(argv[2+arg_offset]), Read(argv[3+arg_offset]));
             inner.execute();
             inner.print();
         }break;
         case(4):{
-            InnerUnity unity( atoi(argv[2]), Read(argv[3]));
+            InnerUnity unity( atoi(argv[1+arg_offset]), Read(argv[2+arg_offset]));
             unity.execute();
             unity.print();
         }break;
