5c3e596b73869a2aeb0b501e04ccc848f23f41d6
DocNos <j.varboncoeur@gmail.com>
2025-11-10 12:26:11 -0800

    heirarchy

---
+++ Projects/proj5/src/pluck.cpp
@@ -0,0 +1,1 @@
+#include "pluck.h"
---
+++ Projects/proj5/src/pluck.h
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <fstream>
+#include <queue>
+#include <random> 
+#include <cmath>
+#include <vector>
+using namespace std;
+
+
+struct fileheader
+{
+	char riff_label[4]; // (00) = {'R','I','F','F'}
+	unsigned riff_size; // (04) = 36 + data_size
+	char file_tag[4]; // (08) = {'W','A','V','E'}
+	char fmt_label[4]; // (12) = {'f','m','t',' '}
+	unsigned fmt_size; // (16) = 16
+	unsigned short audio_format; // (20) = 1
+	unsigned short channel_count; // (22) = 1 or 2
+	unsigned sampling_rate; // (24) = (anything)
+	unsigned bytes_per_second; // (28) = (see above)
+	unsigned short bytes_per_sample; // (32) = (see above)
+	unsigned short bits_per_sample; // (34) = 8 or 16
+	char data_label[4]; // (36) = {'d','a','t','a'}
+	unsigned data_size; // (40) = # bytes of data
+};
+
+struct WavHeader 
+{
+       char chunkID[4];        // "RIFF"
+       uint32_t chunkSize;     // File size - 8
+       char format[4];         // "WAVE"
+       char subchunk1ID[4];    // "fmt "
+       uint32_t subchunk1Size; // 16 for PCM
+       uint16_t audioFormat;   // 1 for PCM
+       uint16_t numChannels;   // 1 for mono
+       uint32_t sampleRate;    // 44100
+       uint32_t byteRate;      // sampleRate * numChannels * bitsPerSample/8
+       uint16_t blockAlign;    // numChannels * bitsPerSample/8
+       uint16_t bitsPerSample; // 16
+       char subchunk2ID[4];    // "data"
+       uint32_t subchunk2Size; // NumSamples * numChannels * bitsPerSample/8
+};
