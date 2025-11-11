2cdc1892aebd3d3f56f0d13c3d10b57a18061b96
DocNos <j.varboncoeur@gmail.com>
2025-11-10 19:28:49 -0800

    header struct added to ctor

--- Projects/proj5/src/pluck.h
+++ Projects/proj5/src/pluck.h
@@ -6,7 +6,61 @@
 #include <vector>
 using namespace std;
 
+struct WavHeader 
+{
+       char chunkID[4];        // "RIFF"
+       uint32_t chunkSize;     // File size - 8
+       char format[4];         // "WAVE"
+       char fmtLabel[4];       // "fmt "
+       uint32_t fmtSize;       // 16 for PCM
+       uint16_t audioFormat;   // 1 for PCM
+       uint16_t numChannels;   // 1 for mono
+       uint32_t sampleRate;    // 44100
+       uint16_t bitsPerSample; // 16
+       uint32_t byteRate;      // sampleRate * numChannels * bitsPerSample/8
+       uint16_t blockAlign;    // numChannels * bitsPerSample/8
+       char subchunk2ID[4];    // "data"
+       uint32_t dataSize;      // NumSamples * numChannels * bitsPerSample/8
+};
 
+class Pluck
+{
+private:
+    WavHeader header_;
+
+public:
+    Pluck()
+    {
+        header_ = createDefault()
+    }
+
+    WavHeader createDefault(unsigned fileSize, unsigned numChannels
+        , unsigned numSamples, unsigned sampleRate, unsigned bitsSample)
+    {
+        unsigned byteRate = sampleRate * numChannels * bitsSample/8;
+        unsigned blockAlignment = numChannels * bitsSample/8;
+        unsigned totalSize = numSamples * numChannels * bitsSample/8;
+        WavHeader header = {
+            {'R', 'I', 'F', 'F'}   // Chunk ID
+            , fileSize             // File size (8)
+            , {'W', 'A', 'V', 'E'} // File Format
+            , {'f', 'm', 't', ' '} // sub-chunk ID
+            , 16                   // sub-chunk size
+            , 1                    // Audio Format (??)
+            , numChannels          // Mono: 1 Channel
+            , sampleRate           // Sample rate
+            , bitsSample           // 
+            , byteRate             // ??       
+            , blockAlignment       // (??) How to fit 
+            , {'d', 'a', 't', 'a'} // this data is made out of data
+            , totalSize
+        };
+
+    }
+
+};
+
+/*
 struct fileheader
 {
 	char riff_label[4]; // (00) = {'R','I','F','F'}
@@ -24,19 +78,5 @@ struct fileheader
 	unsigned data_size; // (40) = # bytes of data
 };
 
-struct WavHeader 
-{
-       char chunkID[4];        // "RIFF"
-       uint32_t chunkSize;     // File size - 8
-       char format[4];         // "WAVE"
-       char subchunk1ID[4];    // "fmt "
-       uint32_t subchunk1Size; // 16 for PCM
-       uint16_t audioFormat;   // 1 for PCM
-       uint16_t numChannels;   // 1 for mono
-       uint32_t sampleRate;    // 44100
-       uint32_t byteRate;      // sampleRate * numChannels * bitsPerSample/8
-       uint16_t blockAlign;    // numChannels * bitsPerSample/8
-       uint16_t bitsPerSample; // 16
-       char subchunk2ID[4];    // "data"
-       uint32_t subchunk2Size; // NumSamples * numChannels * bitsPerSample/8
-};
+
+*/
